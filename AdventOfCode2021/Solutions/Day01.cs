ed27adbee3a58bef070d43c61abf265ad6d1b9b5
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2021-12-01 12:58:15 +0000

    Add project files.

---
+++ AdventOfCode2021/Solutions/Day01.cs
@@ -0,0 +1,73 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2021.Solutions
+{
+    internal class Day01
+    {
+        public int Part1(string[] input)
+        {
+            var numbers = input.Select(x=> Convert.ToInt32(x)).ToList();
+            var linkedList = new LinkedList<int>(numbers);
+            var node = linkedList.First;
+                        
+            var count = 0;
+
+            while(node != null)
+            {
+                if(node.Next == null)
+                {
+                    break;
+                }
+
+                if(node.Value < node.Next.Value)
+                {
+                    count++;
+                }
+                node = node.Next;
+            }
+            return count;
+        }
+
+        internal int Part2(string[] input)
+        {
+            var numbers = input.Select(x => Convert.ToInt32(x)).ToList();
+            var linkedList = new LinkedList<int>(numbers);
+            var node = linkedList.First;
+                        
+            var windows = new LinkedList<int>();
+
+            while (node != null && node.Next!=null && node.Next.Next != null)
+            {
+                var window = node.Value + node.Next.Value + node.Next.Next.Value;
+                windows.AddLast(window);
+
+                node = node.Next;
+            }
+
+
+            node = windows.First;
+
+            var count = 0;
+
+            while (node != null)
+            {
+                if (node.Next == null)
+                {
+                    break;
+                }
+
+                if (node.Value < node.Next.Value)
+                {
+                    count++;
+                }
+                node = node.Next;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ AdventOfCode2021/Tests/Day01Tests.cs
@@ -0,0 +1,58 @@
+﻿using AdventOfCode2021.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace AdventOfCode2021.Tests
+{
+ 
+    [TestClass]
+    public class Day01Tests
+    {
+        private Day01 _day01;
+        string[] Input => File.ReadAllLines(@"Input\Day01.txt");
+
+        private string[] _sample = new string[]
+        {
+            "199",
+            "200",
+            "208",
+            "210",
+            "200",
+            "207",
+            "240",
+            "269",
+            "260",
+            "263",
+        };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day01 = new Day01();
+        }
+
+        [TestMethod]
+        public void Part1_Sample()
+        {
+            Assert.AreEqual(7, _day01.Part1(_sample));
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(1655, _day01.Part1(Input));
+        }
+
+        [TestMethod]
+        public void Part2_Sample()
+        {
+            Assert.AreEqual(5, _day01.Part2(_sample));
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(1683, _day01.Part2(Input));
+        }
+    }
+}
