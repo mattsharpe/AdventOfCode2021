7ee027ca174ef81fd5207a2cc13f6da1ca1d5a15
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2021-12-01 16:57:48 +0000

    Ditching LinkedList in favour of some Linq Goodness.

--- AdventOfCode2021/Solutions/Day01.cs
+++ AdventOfCode2021/Solutions/Day01.cs
@@ -1,73 +1,26 @@
-﻿
-
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
 
 namespace AdventOfCode2021.Solutions
 {
-    internal class Day01
+    public class Day01
     {
         public int Part1(string[] input)
         {
-            var numbers = input.Select(x=> Convert.ToInt32(x)).ToList();
-            var linkedList = new LinkedList<int>(numbers);
-            var node = linkedList.First;
-                        
-            var count = 0;
+            var numbers = input.Select(x => Convert.ToInt32(x)).ToList();
 
-            while(node != null)
-            {
-                if(node.Next == null)
-                {
-                    break;
-                }
+            return numbers.Zip(numbers.Skip(1), (a, b) => b > a).Count(x => x == true);
 
-                if(node.Value < node.Next.Value)
-                {
-                    count++;
-                }
-                node = node.Next;
-            }
-            return count;
         }
 
-        internal int Part2(string[] input)
+        public int Part2(string[] input)
         {
             var numbers = input.Select(x => Convert.ToInt32(x)).ToList();
-            var linkedList = new LinkedList<int>(numbers);
-            var node = linkedList.First;
-                        
-            var windows = new LinkedList<int>();
 
-            while (node != null && node.Next!=null && node.Next.Next != null)
-            {
-                var window = node.Value + node.Next.Value + node.Next.Next.Value;
-                windows.AddLast(window);
+            var windows = numbers.Zip(numbers.Skip(1), (a, b) => a + b)
+                .Zip(numbers.Skip(2), (a, b) => a + b);
 
-                node = node.Next;
-            }
-
-
-            node = windows.First;
-
-            var count = 0;
-
-            while (node != null)
-            {
-                if (node.Next == null)
-                {
-                    break;
-                }
-
-                if (node.Value < node.Next.Value)
-                {
-                    count++;
-                }
-                node = node.Next;
-            }
-
-            return count;
-        }
+            return windows.Zip(windows.Skip(1), (a, b) => b > a).Count(x => x == true);
+        }      
     }
 }
