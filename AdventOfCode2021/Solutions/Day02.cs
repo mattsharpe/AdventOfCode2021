37a222f21625d71a92e7ed8f7ae52ea7963d4f86
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2021-12-02 21:59:09 +0000

    Day 2

---
+++ AdventOfCode2021/Solutions/Day02.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2021.Solutions
+{
+    public class Day02
+    {
+        public int Part1(string[] input)
+        {
+            var displacement = input.Select(x =>  (x.Split(' ')[0], int.Parse(x.Split(' ')[1])))
+                .GroupBy(x => x.Item1,x=>x.Item2)
+                .ToDictionary(x=> x.Key, x=>x.Sum());
+
+            return displacement["forward"] * (displacement["down"] - displacement["up"]);
+
+        }
+
+        public int Part2(string[] input)
+        {
+            var (x, depth, aim) = (0, 0, 0);
+            
+            foreach (var instruction in input)
+            {
+                var split = instruction.Split(' ');
+                var value = int.Parse(split[1]);
+
+                switch (split[0])
+                {
+                    case "forward":
+                        x += value;
+                        depth += aim * value;
+                        break;
+                    case "up":
+                        aim -= value;
+                        break;
+                    case "down":
+                        aim += value;
+                        break;
+                    default:
+                        throw new Exception("Unknown command");
+                }
+            }
+
+            return x * depth;
+        }
+    }   
+}
--- AdventOfCode2021/Tests/Day02Tests.cs
+++ AdventOfCode2021/Tests/Day02Tests.cs
@@ -6,53 +6,49 @@ namespace AdventOfCode2021.Tests
 {
  
     [TestClass]
-    public class Day01Tests
+    public class Day02Tests
     {
-        private Day01 _day01 = new();
-        string[] Input => File.ReadAllLines(@"Input\Day01.txt");
+        private Day02 _day02 = new();
+        string[] Input => File.ReadAllLines(@"Input\Day02.txt");
 
         private string[] _sample = new string[]
         {
-            "199",
-            "200",
-            "208",
-            "210",
-            "200",
-            "207",
-            "240",
-            "269",
-            "260",
-            "263",
+            "forward 5",
+            "down 5",
+            "forward 8",
+            "up 3",
+            "down 8",
+            "forward 2",
         };
 
         [TestInitialize]
         public void Initialize()
         {
-            _day01 = new Day01();
+            _day02 = new Day02();
         }
 
         [TestMethod]
         public void Part1_Sample()
         {
-            Assert.AreEqual(7, _day01.Part1(_sample));
+            Assert.AreEqual(150, _day02.Part1(_sample));
         }
 
         [TestMethod]
         public void Part1()
         {
-            Assert.AreEqual(1655, _day01.Part1(Input));
+            Assert.AreEqual(2102357, _day02.Part1(Input));
         }
 
         [TestMethod]
         public void Part2_Sample()
         {
-            Assert.AreEqual(5, _day01.Part2(_sample));
+            Assert.AreEqual(900, _day02.Part2(_sample));
         }
 
         [TestMethod]
         public void Part2()
         {
-            Assert.AreEqual(1683, _day01.Part2(Input));
+            Assert.AreEqual(2101031224, _day02.Part2(Input));
         }
     }
 }
