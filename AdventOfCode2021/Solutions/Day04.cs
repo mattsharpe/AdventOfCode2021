e85089afb605f5b3ebac8e43a7ab83cabc9806f3
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2021-12-03 22:31:10 +0000

    Day 4 setup

---
+++ AdventOfCode2021/Solutions/Day04.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2021.Solutions
+{
+    public class Day04
+    {
+        public int Part1(string[] input)
+        {
+            return 0;
+        }
+
+        public int Part2(string[] input)
+        {
+            return 0;
+        }
+    }   
+}
---
+++ AdventOfCode2021/Tests/Day04Tests.cs
@@ -0,0 +1,48 @@
+﻿using AdventOfCode2021.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace AdventOfCode2021.Tests
+{
+ 
+    [TestClass]
+    public class Day04Tests
+    {
+        private Day04 _day04 = new();
+        string[] Input => File.ReadAllLines(@"Input\Day04.txt");
+
+        private string[] _sample = new string[]
+        {
+        };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day04 = new Day04();
+        }
+
+        [TestMethod]
+        public void Part1_Sample()
+        {
+            Assert.AreEqual(0, _day04.Part1(_sample));
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(0, _day04.Part1(Input));
+        }
+
+        [TestMethod]
+        public void Part2_Sample()
+        {
+            Assert.AreEqual(0, _day04.Part2(_sample));
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(0, _day04.Part2(Input));
+        }
+    }
+}
