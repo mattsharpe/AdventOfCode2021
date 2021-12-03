14cb265aec32bd41a2b4eb68a17ded7613b072d5
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2021-12-03 15:38:55 +0000

    Day 3 part 1

---
+++ AdventOfCode2021/Solutions/Day03.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2021.Solutions
+{
+    public class Day03
+    {
+        public int Part1(string[] input)
+        {
+            var length = input[0].Length;
+            var result = new StringBuilder();
+
+            for(int i = 0; i < length; i++)
+            {
+                var count = input.Count(x => x[i] == '1');
+                var bit = count > input.Length / 2;
+                result.Append(bit ? '1' : '0');
+            }
+
+            var resultInverted = string.Join("", result.ToString().Select(x => x == '0' ? '1' : '0'));
+            var gammaRate =  Convert.ToInt32(result.ToString(), 2);
+            var epsilonRate = Convert.ToInt32(resultInverted, 2);                     
+
+            return gammaRate * epsilonRate;
+
+        }
+
+        public int Part2(string[] input)
+        {
+            return 0;
+        }
+    }   
+}
---
+++ AdventOfCode2021/Tests/Day03Tests.cs
@@ -0,0 +1,60 @@
+﻿using AdventOfCode2021.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace AdventOfCode2021.Tests
+{
+ 
+    [TestClass]
+    public class Day03Tests
+    {
+        private Day03 _day03 = new();
+        string[] Input => File.ReadAllLines(@"Input\Day03.txt");
+
+        private string[] _sample = new string[]
+        {
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010",
+        };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day03 = new Day03();
+        }
+
+        [TestMethod]
+        public void Part1_Sample()
+        {
+            Assert.AreEqual(198, _day03.Part1(_sample));
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(3633500, _day03.Part1(Input));
+        }
+
+        [TestMethod]
+        public void Part2_Sample()
+        {
+            Assert.AreEqual(123, _day03.Part2(_sample));
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(123, _day03.Part2(Input));
+        }
+    }
+}
