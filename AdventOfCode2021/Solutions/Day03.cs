086bf25ee5ef1c4b82a535a3eed3ea46278c0a52
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2021-12-03 22:23:28 +0000

    Day 3 - part 2

--- AdventOfCode2021/Solutions/Day03.cs
+++ AdventOfCode2021/Solutions/Day03.cs
@@ -9,27 +9,53 @@ namespace AdventOfCode2021.Solutions
     {
         public int Part1(string[] input)
         {
-            var length = input[0].Length;
-            var result = new StringBuilder();
+            var result = ProcessList(new List<string>(input));
 
-            for(int i = 0; i < length; i++)
-            {
-                var count = input.Count(x => x[i] == '1');
-                var bit = count > input.Length / 2;
-                result.Append(bit ? '1' : '0');
-            }
-
-            var resultInverted = string.Join("", result.ToString().Select(x => x == '0' ? '1' : '0'));
+            var resultInverted = Invert(result);
             var gammaRate =  Convert.ToInt32(result.ToString(), 2);
             var epsilonRate = Convert.ToInt32(resultInverted, 2);                     
 
             return gammaRate * epsilonRate;
+        }
 
+        public string ProcessList(List<string> list)
+        {
+            var result = new StringBuilder();
+
+            for (int i = 0; i < list[0].Length; i++)
+            {
+                var count = list.Count(x => x[i] == '1');
+                var bit = 2 * count >= list.Count;
+                result.Append(bit ? '1' : '0');
+            }
+
+            return result.ToString();
+        }
+
+        public string Invert(string bits)
+        {
+            return string.Join("", bits.ToString().Select(x => x == '0' ? '1' : '0'));
+        }
+
+        public int FilterList(List<string> list, bool inverted = false)
+        {
+            var count = list.Count();
+            for (int i = 0; i < count; i++)
+            {
+                var most = inverted ? Invert(ProcessList(list)) : ProcessList(list);
+                list.RemoveAll(x => x[i] != most[i]);
+                if (list.Count == 1) break;
+            }
+
+            return Convert.ToInt32(list.Single().ToString(), 2);
         }
 
         public int Part2(string[] input)
         {
-            return 0;
+            var co2GeneratorRating = FilterList(new List<string>(input));
+            var co2ScrubberRating = FilterList(new List<string>(input), true);
+
+            return co2GeneratorRating * co2ScrubberRating;
         }
     }   
 }
--- AdventOfCode2021/Tests/Day03Tests.cs
+++ AdventOfCode2021/Tests/Day03Tests.cs
@@ -48,13 +48,13 @@ namespace AdventOfCode2021.Tests
         [TestMethod]
         public void Part2_Sample()
         {
-            Assert.AreEqual(123, _day03.Part2(_sample));
+            Assert.AreEqual(230, _day03.Part2(_sample));
         }
 
         [TestMethod]
         public void Part2()
         {
-            Assert.AreEqual(123, _day03.Part2(Input));
+            Assert.AreEqual(4550283, _day03.Part2(Input));
         }
     }
 }
