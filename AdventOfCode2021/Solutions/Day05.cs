8841332b7afd41658b78379bbdffbdeda29e764f
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2021-12-06 07:44:42 +0000

    Hopping machines

---
+++ AdventOfCode2021/Solutions/Day05.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2021.Solutions
+{
+    public class Day05
+    {
+        public int Part1(string[] input)
+        {
+            var ranges = ParseInput(input).ToArray();
+
+            var max = input.SelectMany(x=> Regex.Matches(x, @"\d+").Select(x=> int.Parse(x.Value))).Max();
+
+            var maxX = ranges.Select(x => x.x1).Union(ranges.Select(x => x.x2)).Max();
+            var maxY = ranges.Select(x => x.y1).Union(ranges.Select(x => x.y2)).Max();
+
+            Console.WriteLine(max + ": " + maxX + "," + maxY);
+            return 0;
+        }
+
+        public int Part2(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CoordinateRange> ParseInput(string[] input)
+        {
+            var regex = new Regex(@"(\d+),(\d+) -> (\d+),(\d+)");
+            foreach(var line in input)
+            {
+                var groups = regex.Matches(line)[0].Groups;
+                yield return new CoordinateRange(
+                    int.Parse(groups[1].Value),
+                    int.Parse(groups[2].Value),
+                    int.Parse(groups[3].Value),
+                    int.Parse(groups[4].Value)
+                    );
+            }
+        }
+
+
+    }
+
+    public record CoordinateRange(int x1, int y1, int x2, int y2);
+}
---
+++ AdventOfCode2021/Tests/Day05Tests.cs
@@ -0,0 +1,58 @@
+﻿using AdventOfCode2021.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace AdventOfCode2021.Tests
+{
+ 
+    [TestClass]
+    public class Day05Tests
+    {
+        private Day05 _day05 = new();
+        string[] Input => File.ReadAllLines(@"Input\Day05.txt");
+
+        private string[] _sample = new string[]
+        {
+            "0,9 -> 5,9",
+            "8,0 -> 0,8",
+            "9,4 -> 3,4",
+            "2,2 -> 2,1",
+            "7,0 -> 7,4",
+            "6,4 -> 2,0",
+            "0,9 -> 2,9",
+            "3,4 -> 1,4",
+            "0,0 -> 8,8",
+            "5,5 -> 8,2",
+        };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day05 = new Day05();
+        }
+
+        [TestMethod]
+        public void Part1_Sample()
+        {
+            Assert.AreEqual(5, _day05.Part1(_sample));
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(0, _day05.Part1(Input));
+        }
+
+        [TestMethod]
+        public void Part2_Sample()
+        {
+            Assert.AreEqual(0, _day05.Part2(_sample));
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(0, _day05.Part2(Input));
+        }
+    }
+}
