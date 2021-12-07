09773476f7c95fcd4919bc030034e88cad48da05
mattsharpe <mattsharpe@users.noreply.github.com>
2021-12-07 10:19:09 +0000

    Day 7

---
+++ AdventOfCode2021/Solutions/Day07.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2021.Solutions
+{
+    public class Day07
+    {
+        public int Part1(string input)
+        {
+            var positions = input.Split(',').Select(int.Parse).ToList(); 
+            return Enumerable.Range(positions.Min(), positions.Max()).
+                Min(x => positions.Select(pos => Math.Abs(pos - x)).Sum());
+        }
+
+        public int Part2(string input)
+        {
+            // Calculate the sum of a linear progression using the formula
+            // n/2 * (2a + (n − 1) * d)
+            // (with some simplifications as d = a = 1)
+
+            var positions = input.Split(',').Select(int.Parse).ToList();
+            return Enumerable.Range(positions.Min(), positions.Max()).
+                Min(x => positions.Select(pos => {  
+                    var n = Math.Abs(pos - x);
+                    return Convert.ToInt32(n * 0.5 * (1 + n));
+                }).Sum());
+        }
+    }
+}
---
+++ AdventOfCode2021/Tests/Day07Tests.cs
@@ -0,0 +1,46 @@
+﻿using AdventOfCode2021.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace AdventOfCode2021.Tests
+{
+ 
+    [TestClass]
+    public class Day07Tests
+    {
+        private Day07 _day07 = new();
+        string Input => File.ReadAllText(@"Input\Day07.txt");
+
+        private string _sample = "16,1,2,0,4,2,7,1,2,14";
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day07 = new Day07();
+        }
+
+        [TestMethod]
+        public void Part1_Sample()
+        {
+            Assert.AreEqual(37, _day07.Part1(_sample));
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(345197, _day07.Part1(Input));
+        }
+
+        [TestMethod]
+        public void Part2_Sample()
+        {
+            Assert.AreEqual(168, _day07.Part2(_sample));
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(96361606, _day07.Part2(Input));
+        }
+    }
+}
