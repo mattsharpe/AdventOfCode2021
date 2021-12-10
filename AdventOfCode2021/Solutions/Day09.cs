ac74ed5688c2b7de814d139abdbec6a1c622bd29
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2021-12-10 17:00:53 +0000

    Day 9 part 1

---
+++ AdventOfCode2021/Solutions/Day09.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace AdventOfCode2021.Solutions
+{
+    public class Day09
+    {
+        public int Part1(string[] input)
+        {
+            var map = ParseInput(input);
+            
+            List<Vector2> GetAdjacent(Vector2 cell)
+            {
+                //this syntax is awesome
+                return new List<Vector2>
+                {
+                    cell with {Y = cell.Y + 1},
+                    cell with {Y = cell.Y - 1},
+                    cell with {X = cell.X + 1},
+                    cell with {X = cell.X - 1},
+                };
+            }
+
+            var count = map.Where(
+                    cell => GetAdjacent(cell.Key)
+                        .Where(map.ContainsKey)
+                        .Select(x => map[x])
+                        .All(adj => adj > cell.Value))
+                .Sum(cell => cell.Value + 1);
+
+            return count;
+        }
+
+        public int Part2(string[] input)
+        {
+
+            return 0;
+        }
+
+        public Dictionary<Vector2, int> ParseInput(string[] input)
+        {
+            var map = new Dictionary<Vector2, int>();
+            for (var y = 0; y < input.Length; y++)
+            {
+                for (var x = 0; x < input[y].Length; x++)
+                {
+                    map.Add(new Vector2(x, y), Convert.ToInt32(input[y][x] + ""));
+                }
+            }
+
+            return map;
+        }
+    }
+}
---
+++ AdventOfCode2021/Tests/Day09Tests.cs
@@ -0,0 +1,53 @@
+﻿using AdventOfCode2021.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace AdventOfCode2021.Tests
+{
+ 
+    [TestClass]
+    public class Day09Tests
+    {
+        private Day09 _day09 = new();
+        string[] Input => File.ReadAllLines(@"Input\Day09.txt");
+
+        private string[] _sample = 
+        {
+            "2199943210",
+            "3987894921",
+            "9856789892",
+            "8767896789",
+            "9899965678",
+        };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day09 = new Day09();
+        }
+
+        [TestMethod]
+        public void Part1_Sample()
+        {
+            Assert.AreEqual(15, _day09.Part1(_sample));
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(475, _day09.Part1(Input));
+        }
+
+        [TestMethod]
+        public void Part2_Sample()
+        {
+            Assert.AreEqual(0, _day09.Part2(_sample));
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(0, _day09.Part2(Input));
+        }
+    }
+}
