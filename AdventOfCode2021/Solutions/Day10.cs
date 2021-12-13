facb7a1aaacd090f8e80828f53a3548bcf988c16
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2021-12-13 20:33:03 +0000

    'Syntax error in navigation subsystem on line: all of them'

---
+++ AdventOfCode2021/Solutions/Day10.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2021.Solutions
+{
+    public class Day10
+    {
+        public int Part1(string[] input)
+        {
+            return input.Sum(CorruptionScore);
+        }
+
+        public int Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public int CorruptionScore(string line)
+        {
+            var bracketPairs = new BracketPair[]
+            {
+                new ('{','}'),
+                new ('(',')'),
+                new ('[',']'),
+                new ('<','>'),
+            };
+            
+            var stack = new Stack<char>();
+            foreach (var bracket in line)
+            {
+                if (bracketPairs.Select(x=>x.Open).ToHashSet().Contains(bracket))
+                {
+                    stack.Push(bracket);
+                    
+                }
+                else
+                {
+                    var opener = stack.Pop();
+                    var expected = bracketPairs.Single(x => x.Open == opener);
+                    if (expected.Close == bracket) continue;
+
+                    Console.WriteLine($"Expected {expected.Close}, but found {bracket} instead");
+                    return bracket switch
+                    {
+                        ')' => 3,
+                        ']' => 57,
+                        '}' => 1197,
+                        '>' => 25137,
+                        _ => 0
+                    };
+                }
+            }
+
+            return 0;
+        }
+
+
+    }
+
+    record BracketPair(char Open, char Close);
+}
---
+++ AdventOfCode2021/Tests/Day10Tests.cs
@@ -0,0 +1,58 @@
+﻿using AdventOfCode2021.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace AdventOfCode2021.Tests
+{
+ 
+    [TestClass]
+    public class Day10Tests
+    {
+        private Day10 _day10 = new();
+        string[] Input => File.ReadAllLines(@"Input\Day10.txt");
+
+        private string[] _sample = 
+        {
+            "[({(<(())[]>[[{[]{<()<>>",
+            "[(()[<>])]({[<{<<[]>>(",
+            "{([(<{}[<>[]}>{[]{[(<()>",
+            "(((({<>}<{<{<>}{[]{[]{}",
+            "[[<[([]))<([[{}[[()]]]",
+            "[{[{({}]{}}([{[{{{}}([]",
+            "{<[[]]>}<{[{[{[]{()[[[]",
+            "[<(<(<(<{}))><([]([]()",
+            "<{([([[(<>()){}]>(<<{{",
+            "<{([{{}}[<[[[<>{}]]]>[]]",
+        };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day10 = new Day10();
+        }
+
+        [TestMethod]
+        public void Part1_Sample()
+        {
+            Assert.AreEqual(26397, _day10.Part1(_sample));
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(0, _day10.Part1(Input));
+        }
+
+        [TestMethod]
+        public void Part2_Sample()
+        {
+            Assert.AreEqual(0, _day10.Part2(_sample));
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(0, _day10.Part2(Input));
+        }
+    }
+}
