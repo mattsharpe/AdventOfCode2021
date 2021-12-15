1e0f3d2c5db3ab2a0bd596afd11116365d1a9ed0
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2021-12-15 17:52:50 +0000

    Day 12 part 1

---
+++ AdventOfCode2021/Solutions/Day12.cs
@@ -0,0 +1,78 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2021.Solutions
+{
+    public class Day12
+    {
+        public long Part1(string[] input)
+        {
+            var graph = ParseInput(input);
+            return CountPaths(graph);
+
+        }
+        public long Part2(string[] input)
+        {
+            return 0;
+        }
+
+        //must be a way to do this more nicely with linq?
+        private Dictionary<string, List<string>> ParseInput(string[] input)
+        {
+            var result = new Dictionary<string, List<string>>();
+            
+            foreach (var line in input)
+            {
+                var split = line.Split('-');
+
+                var left = result.GetValueOrDefault(split[0]) ?? new List<string>();
+                var right = result.GetValueOrDefault(split[1]) ?? new List<string>();
+
+                left.Add(split[1]);
+                right.Add(split[0]);
+
+                result[split[0]] = left;
+                result[split[1]] = right;
+                
+            }
+
+            return result;
+        }
+
+        private int CountPaths(Dictionary<string, List<string>> map)
+        {
+            //This is hte recursive call - defined as an inline method here
+            IEnumerable<List<string>> ExploreCave(List<string> currentPath, IReadOnlySet<string> visitedCaves)
+            {
+                var currentCave = currentPath.Last();
+                var result = new List<List<string>> { currentPath };
+                
+                //Once we reach the end cave, stop
+                if (currentCave == "end")
+                {
+                    return result;
+                }
+
+                foreach (var cave in map[currentCave].Where(x => x.All(char.IsUpper) || !visitedCaves.Contains(x)))
+                {
+                    var next = new List<string>(currentPath) { cave };
+                    var visited = new HashSet<string>(visitedCaves) { cave };
+                    
+                    result.AddRange(ExploreCave(next, visited));
+                }
+                return result;
+            }
+
+            var start = new[] { "start" };
+            var paths = ExploreCave(start.ToList(), start.ToHashSet())
+                .Where(path => path.Last() == "end");
+
+            return paths.Count();
+        }
+
+        
+
+
+    }
+}
---
+++ AdventOfCode2021/Tests/Day12Tests.cs
@@ -0,0 +1,103 @@
+﻿using AdventOfCode2021.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace AdventOfCode2021.Tests
+{
+ 
+    [TestClass]
+    public class Day12Tests
+    {
+        private Day12 _day12 = new();
+        string[] Input => File.ReadAllLines(@"Input\Day12.txt");
+
+        private readonly string[] _sample = 
+        {
+            "start-A",
+            "start-b",
+            "A-c",
+            "A-b",
+            "b-d",
+            "A-end",
+            "b-end",
+        };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day12 = new Day12();
+        }
+
+        [TestMethod]
+        public void Part1_Sample()
+        {
+            Assert.AreEqual(10, _day12.Part1(_sample));
+        }
+
+        [TestMethod]
+        public void Part1_Sample2()
+        {
+            var input = new[]
+            {
+                "dc-end",
+                "HN-start",
+                "start-kj",
+                "dc-start",
+                "dc-HN",
+                "LN-dc",
+                "HN-end",
+                "kj-sa",
+                "kj-HN",
+                "kj-dc",
+            };
+
+            Assert.AreEqual(19, _day12.Part1(input));
+        }
+
+        [TestMethod]
+        public void Part1_Sample3()
+        {
+            var input = new[]
+            {
+                "fs-end",
+                "he-DX",
+                "fs-he",
+                "start-DX",
+                "pj-DX",
+                "end-zg",
+                "zg-sl",
+                "zg-pj",
+                "pj-he",
+                "RW-he",
+                "fs-DX",
+                "pj-RW",
+                "zg-RW",
+                "start-pj",
+                "he-WI",
+                "zg-he",
+                "pj-fs",
+                "start-RW",
+            };
+
+            Assert.AreEqual(226, _day12.Part1(input));
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(4792, _day12.Part1(Input));
+        }
+
+        [TestMethod]
+        public void Part2_Sample()
+        {
+            Assert.AreEqual(0, _day12.Part2(_sample));
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(0, _day12.Part2(Input));
+        }
+    }
+}
