8d6287b5d6142d647fbeb01e8d6523bb1147b19a
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2022-11-11 23:05:50 +0000

    Day 15: Chiton

---
+++ AdventOfCode2021/Solutions/Day15.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Map = System.Collections.Generic.Dictionary<System.Numerics.Vector2, int>;
+
+namespace AdventOfCode2021.Solutions
+{
+    public class Day15
+    {
+        public int Part1(string[] input)
+        {
+            var map = Parse(input);
+            
+            return FindMinimumRiskPath(map);
+        }
+
+        public int Part2(string[] input)
+        {
+            var smallMap = Parse(input);
+            var fullMap = ExpandMap(smallMap);
+
+            return FindMinimumRiskPath(fullMap);
+        }
+
+        private Map ExpandMap(Map map)
+        {
+
+            var result = new Map();
+            
+            var width = (int)map.Keys.Max(a => a.X) + 1;
+            var height = (int)map.Keys.Max(a => a.Y) + 1;
+
+            foreach (var x in Enumerable.Range(0, width * 5))
+            {
+                foreach (var y in Enumerable.Range(0, height * 5))
+                {
+                    var tileY = y % height;
+                    var tileX = x % width;
+                    var tileRiskLevel = map[new Vector2(tileX, tileY)];
+
+                    var offset = (y / height + x / width) - 1;
+                    
+                    var riskLevel = (tileRiskLevel + offset) % 9 + 1;
+                    result.Add(new Vector2(x,y), riskLevel);
+                }
+            }
+            
+            return result;
+        }
+
+        private int FindMinimumRiskPath(Map map)
+        {
+            var start = new Vector2(0, 0);
+            var end = new Vector2(map.Keys.Max(a=>a.X), map.Keys.Max(a => a.Y));
+
+            var queue = new PriorityQueue<Vector2, int>();
+            var totalRiskMap = new Map
+            {
+                {start, 0}
+            };
+
+            queue.Enqueue(start, 0);
+
+            while (queue.Peek() != end)
+            {
+                var next = queue.Dequeue();
+
+                foreach (var n in Neighbours(next).Where(map.ContainsKey))
+                {
+                    var totalRisk = totalRiskMap[next] + map[n];
+                    
+                    //only bother if this path is lower risk
+                    if (totalRisk >= totalRiskMap.GetValueOrDefault(n, int.MaxValue)) continue;
+
+                    totalRiskMap [n] = totalRisk;
+                    queue.Enqueue(n, totalRisk);
+                }
+            }
+
+            return totalRiskMap [end];
+        }
+
+        public Map Parse(string[] input)
+        {
+            var map = new Map();
+
+            for (var y = 0; y < input.Length; y++)
+            {
+                for (var x = 0; x < input[y].Length; x++)
+                {
+                    map.Add(new Vector2(x, y), Convert.ToInt32(input[y][x] + ""));
+                }
+            }
+            return map;
+        }
+
+        private IEnumerable<Vector2> Neighbours(Vector2 position) =>
+            new[]
+            {
+                position with { X = position.X + 1 },
+                position with { X = position.X - 1 },
+                position with { Y = position.Y + 1 },
+                position with { Y = position.Y - 1 }
+            };
+
+    }
+    
+}
---
+++ AdventOfCode2021/Tests/Day15Tests.cs
@@ -0,0 +1,59 @@
+﻿using AdventOfCode2021.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace AdventOfCode2021.Tests
+{
+ 
+    [TestClass]
+    public class Day15Tests
+    {
+        private Day15 _day15 = new();
+        string[] Input => File.ReadAllLines(@"Input\Day15.txt");
+
+        private readonly string[] _sample = 
+        {
+            "1163751742",
+            "1381373672",
+            "2136511328",
+            "3694931569",
+            "7463417111",
+            "1319128137",
+            "1359912421",
+            "3125421639",
+            "1293138521",
+            "2311944581",
+        };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day15 = new Day15();
+        }
+
+        [TestMethod]
+        public void Part1_Sample()
+        {
+            Assert.AreEqual(40, _day15.Part1(_sample));
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(462, _day15.Part1(Input));
+        }
+
+        [TestMethod]
+        public void Part2_Sample()
+        {
+            Assert.AreEqual(315, _day15.Part2(_sample));
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(2846, _day15.Part2(Input));
+        }
+    }
+
+}
