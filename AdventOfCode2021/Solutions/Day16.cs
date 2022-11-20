5b0f836b4c13b77ea0c44a6d8177b633187f9273
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2022-11-20 10:17:40 +0000

    Day 16: Packet Decoder

---
+++ AdventOfCode2021/Solutions/Day16.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2021.Solutions
+{
+    public class Day16
+    {
+        public int Part1(string input)
+        {
+            var parsed = ParseInput(input);
+
+            return ReadPacket(parsed).AggregateVersion();
+        }
+
+        public long Part2(string input)
+        {
+            var parsed = ParseInput(input);
+            var packet = ReadPacket(parsed);
+            
+            return CalculatePacketValue(packet);
+        }
+
+        long CalculatePacketValue(Packet packet)
+        {
+            long result = 0;
+            var values = packet.ChildPackets.Select(CalculatePacketValue);
+
+            switch (packet.Type)
+            {
+                case 0:
+                    result = packet.ChildPackets.Sum(CalculatePacketValue);
+                    break;
+                case 1:
+                    result = 1;
+                    foreach (var child in packet.ChildPackets)
+                    {
+                        result *= CalculatePacketValue(child);
+                    }
+                    break;
+                case 2:
+                    result = packet.ChildPackets.Min(CalculatePacketValue);
+                    break;
+                case 4:
+                    result = packet.Value;
+                    break;
+                case 3:
+                    result = packet.ChildPackets.Max(CalculatePacketValue);
+                    break;
+                case 5:
+                    result = values.First() > values.Last() ? 1 : 0;
+                    break;
+                case 6:
+                    result = values.First() < values.Last() ? 1 : 0;
+                    break;
+                case 7:
+                    result = values.First() == values.Last() ? 1 : 0;
+                    break;
+
+            }
+
+            return result;
+            
+        }
+
+        private Parser ParseInput(string input)
+        {
+            var binaryString = string.Join("", input.Select(c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')));
+
+            var bits = binaryString.Select(x => int.Parse("" + x)).ToArray();
+
+            return new Parser(bits);
+        }
+
+        Packet ReadPacket(Parser parser)
+        {
+            var version = parser.ReadNumber(3);
+            var type = parser.ReadNumber(3);
+            var packets = new List<Packet>();
+            var value = 0L;
+            
+            if (type == 4)
+            {
+                var bits = "";
+                //This is a literal, we just need to get the value out by reading the next packets until we hit one that starts with 1
+                while (true)
+                {
+                    var final = parser.ReadNumber(1) == 0;
+                    bits += parser.ReadBitString(4);
+
+                    if (!final) continue;
+                    value = Convert.ToInt64(bits, 2);
+                    break;
+                }
+            }
+            else
+            {
+                var lengthType = parser.ReadNumber(1);
+                if (lengthType == 1)
+                {
+                    //read the next 11 bits to get the number of packets to read
+                    var numberOfPackets = parser.ReadNumber(11);
+                    foreach (var x in Enumerable.Range(0, numberOfPackets))
+                    {
+                        packets.Add(ReadPacket(parser));
+                    }
+                }
+                else
+                {
+                    // Read the next 15bits 
+                    var length = parser.ReadNumber(15);
+
+                    // ths is the number of bits that make up our next packet - get a parser for that.
+                    var nestedParser = parser.GetParserForSubPackets(length);
+
+                    while (!nestedParser.Finished)
+                    {
+                        packets.Add(ReadPacket(nestedParser));
+                    }
+                }
+            }
+
+            var result = new Packet
+            {
+                Version = version,
+                Type = type,
+                Value = value,
+                ChildPackets = packets.ToArray()
+            };
+                
+            return result;
+        }
+    }
+}
+
+class Packet
+{
+    public int Version { get; set; }
+    public int Type { get; set; }
+    public long Value { get; set; }
+    public Packet[] ChildPackets { get; set; }
+
+    public int AggregateVersion()
+    {
+        return Version + ChildPackets.Sum(x => x.AggregateVersion());
+    }
+}
+
+class Parser
+{
+    private readonly int[] _bits;
+    private int _currentLocation;
+
+    public bool Finished => _currentLocation == _bits.Length;
+
+    public Parser(int[] bits)
+    {
+        _bits = bits;
+    }
+    
+    public Parser GetParserForSubPackets(int length)
+    {
+        var result = new Parser(_bits.Skip(_currentLocation).Take(length).ToArray());
+        _currentLocation += length;
+
+        return result;
+    }
+
+    public int ReadNumber(int length)
+    {
+        var binaryString = string.Join("", _bits.Skip(_currentLocation).Take(length));
+        _currentLocation += length;
+
+        return Convert.ToInt32(binaryString, 2);
+    }
+
+    public string ReadBitString(int length)
+    {
+        var binaryString = string.Join("", _bits.Skip(_currentLocation).Take(length));
+        _currentLocation += length;
+
+        return binaryString;
+    }
+
+    public int Peek()
+    {
+        return _bits.Skip(_currentLocation).Take(1).Single();
+    }
+}
+
+
---
+++ AdventOfCode2021/Tests/Day16Tests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using AdventOfCode2021.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2021.Tests
+{
+ 
+    [TestClass]
+    public class Day16Tests
+    {
+        private Day16 _day16 = new();
+        string Input => File.ReadAllText(@"Input\Day16.txt");
+
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day16 = new Day16();
+        }
+
+        [DataTestMethod]
+        [DataRow("D2FE28", 6)]
+        [DataRow("8A004A801A8002F478",16)]
+        [DataRow("620080001611562C8802118E34", 12)]
+        [DataRow("C0015000016115A2E0802F182340", 23)]
+        [DataRow("A0016C880162017C3686B18A3D4780", 31)]
+        public void Part1_Sample(string input, int expected)
+        {
+            Assert.AreEqual(expected, _day16.Part1(input));
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(949, _day16.Part1(Input));
+        }
+
+        [DataTestMethod]
+        [DataRow("C200B40A82", 3)]
+        [DataRow("04005AC33890", 54)]
+        [DataRow("880086C3E88112", 7)]
+        [DataRow("CE00C43D881120", 9)]
+        [DataRow("D8005AC2A8F0", 1)]
+        [DataRow("F600BC2D8F", 0)]
+        [DataRow("9C005AC2F8F0", 0)]
+        [DataRow("9C0141080250320F1802104A08", 1)]
+        public void Part2_Sample(string input, int expected)
+        {
+            Assert.AreEqual(expected, _day16.Part2(input));
+        }
+
+
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(1114600142730, _day16.Part2(Input));
+        }
+    }
+
+}
