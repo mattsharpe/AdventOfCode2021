d4cb34ea6a97ba116ba9f93660a3b3b7bf6b4663
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2022-11-24 11:43:34 +0000

    Day 17: Trick Shot

---
+++ AdventOfCode2021/Solutions/Day17.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2021.Solutions
+{
+    public class Day17
+    {
+
+        public int Part1(string input)
+        {
+            var range = Parse(input);
+
+            return MaxAltitudes(range).Max();
+        }
+
+
+        public int Part2(string input)
+        {
+            var range = Parse(input);
+
+            return MaxAltitudes(range).Count();
+        }
+
+        private IEnumerable<int> MaxAltitudes(TargetRange range)
+        {
+            for (var x = 0; x <= range.MaxX; x++)
+            {
+                for (var y = range.MinY; y <= Math.Abs(range.MinY); y++)
+                {
+                    var state = new ProbeState(0, 0, x, y);
+                    var maxY = 0;
+
+                    //while we're still approaching the target
+                    while (state.LocationX <= range.MaxX && state.LocationY >= range.MinY)
+                    {
+                        state = Move(state);
+                        maxY = Math.Max(maxY, state.LocationY);
+
+                        //if we're still approaching the target then continue
+                        if (state.LocationX < range.MinX || state.LocationX > range.MaxX ||
+                            state.LocationY < range.MinY || state.LocationY > range.MaxY) continue;
+
+                        yield return maxY;
+                        break;
+                    }
+                }
+            }
+
+        }
+        
+
+        private ProbeState Move(ProbeState state)
+        {
+            var nextX = state.LocationX + state.VelocityX;
+            var nextY = state.LocationY + state.VelocityY;
+            var nextVelocityX = Math.Max(0, state.VelocityX - 1);
+            var nextVelocityY = state.VelocityY - 1;
+
+            return new ProbeState(nextX, nextY, nextVelocityX, nextVelocityY);
+        }
+
+        private TargetRange Parse(string input)
+        {
+            var regex = new Regex(@"x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)");
+            var groups = regex.Match(input).Groups;
+
+            return new TargetRange(
+                Convert.ToInt32(groups[1].Value),
+                Convert.ToInt32(groups[2].Value),
+                Convert.ToInt32(groups[3].Value),
+                Convert.ToInt32(groups[4].Value)
+            );
+        }
+    }
+    record TargetRange(int MinX, int MaxX, int MinY, int MaxY);
+    record ProbeState(int LocationX, int LocationY, int VelocityX, int VelocityY);
+}
---
+++ AdventOfCode2021/Tests/Day17Tests.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using AdventOfCode2021.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode2021.Tests
+{
+    [TestClass]
+    public class Day17Tests
+    {
+        private Day17 _day17 = new();
+        string Input => File.ReadAllText(@"Input\Day17.txt");
+        private string _sample = "target area: x=20..30, y=-10..-5";
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day17 = new Day17();
+        }
+
+        [TestMethod]
+        public void Part1_Sample()
+        {
+            Assert.AreEqual(45, _day17.Part1(_sample));
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(8646, _day17.Part1(Input));
+        }
+
+        [TestMethod]
+        public void Part2_Sample()
+        {
+            Assert.AreEqual(112, _day17.Part2(_sample));
+        }
+
+
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(5945, _day17.Part2(Input));
+        }
+
+    }
+}
